61baeaa3c67c1d1e043d325a350d4f95e110d500
Tibi <andercoiosif@gmail.com>
2021-04-14 20:54:26 +0300

    update

--- src/GamePanel.java
+++ src/GamePanel.java
@@ -12,14 +12,14 @@ public class GamePanel extends JPanel implements ActionListener {
     static final int SCREEN_HEIGHT = 600;
     static final int UNIT_SIZE = 25;
     static final int GAME_UNITS = (SCREEN_WIDTH * SCREEN_HEIGHT) / (UNIT_SIZE*UNIT_SIZE);
-    static final int DELAY = 175;
+    static final int DELAY = 80;
     final int[] x = new int[GAME_UNITS];
     final int[] y = new int[GAME_UNITS];
     int bodyParts = 6;
-    int appleeEaten;
+    int appleeEaten = 0;
     int appleX;
     int appleY;
-    char direction = 'R';
+    static char direction = 'R';
     boolean running = false;
     Timer timer;
     Random random;
@@ -46,8 +46,23 @@ public class GamePanel extends JPanel implements ActionListener {
     }
 
     public void draw(Graphics g) {
-        g.setColor(Color.RED);
-        g.fillOval(appleX, appleY, UNIT_SIZE, UNIT_SIZE);
+        if(running) {
+            g.setColor(Color.RED);
+            g.fillOval(appleX, appleY, UNIT_SIZE, UNIT_SIZE);
+
+            for (int i = 0; i < bodyParts; i++) {
+                if (i == 0) {
+                    g.setColor(Color.CYAN);
+                    g.fillRect(x[i], y[i], UNIT_SIZE, UNIT_SIZE);
+                } else {
+                    g.setColor(Color.GREEN);
+                    g.fillRect(x[i], y[i], UNIT_SIZE, UNIT_SIZE);
+                }
+            }
+
+        } else {
+            gameOver(g);
+        }
     }
 
     public void newApple() {
@@ -57,30 +72,93 @@ public class GamePanel extends JPanel implements ActionListener {
     }
 
     public void move() {
-
+        for(int i = bodyParts; i > 0; i--){
+            x[i] = x[i-1];
+            y[i] = y[i-1];
+        }
+        switch (direction) {
+            case 'U' -> y[0] = y[0] - UNIT_SIZE;
+            case 'D' -> y[0] = y[0] + UNIT_SIZE;
+            case 'L' -> x[0] = x[0] - UNIT_SIZE;
+            case 'R' -> x[0] = x[0] + UNIT_SIZE;
+        }
     }
 
     public void checkApple() {
-
+        if(appleX == x[0] && appleY == y[0]){
+            appleeEaten ++;
+            bodyParts ++;
+            newApple();
+        }
     }
 
     public void checkCollisions() {
-
+        for(int i = bodyParts; i > 0; i--){
+            if (x[0] == x[i] && y[0] == y[i]) {
+                running = false;
+                break;
+            }
+        }
+        if(x[0] < 0){
+            running = false;
+        }
+        if(x[0] > SCREEN_WIDTH){
+            running = false;
+        }
+        if(y[0] < 0){
+            running = false;
+        }
+        if(y[0] > SCREEN_HEIGHT){
+            running = false;
+        }
+        if(!running){
+            timer.stop();
+        }
     }
 
-    public void gameOver() {
-
+    public void gameOver(Graphics g) {
+        g.setColor(Color.YELLOW);
+        g.setFont(new Font("Consolas", Font.PLAIN,75));
+        FontMetrics metrics = getFontMetrics(g.getFont());
+        g.drawString("Game Over", (SCREEN_WIDTH - metrics.stringWidth("Game Over")) / 2, SCREEN_HEIGHT/2);
+        g.drawString("Score:" + appleeEaten, (SCREEN_WIDTH - metrics.stringWidth("Score:" + appleeEaten)) / 2, 75);
     }
 
     public static class MyKeyAdapter extends KeyAdapter {
         @Override
         public void keyPressed(KeyEvent e){
-
+            switch (e.getKeyCode()){
+                case KeyEvent.VK_LEFT:
+                    if (direction != 'R'){
+                        direction = 'L';
+                    }
+                    break;
+                case KeyEvent.VK_RIGHT:
+                    if (direction != 'L'){
+                        direction = 'R';
+                    }
+                    break;
+                case KeyEvent.VK_UP:
+                    if (direction != 'D'){
+                        direction = 'U';
+                    }
+                    break;
+                case KeyEvent.VK_DOWN:
+                    if (direction != 'U'){
+                        direction = 'D';
+                    }
+                    break;
+            }
         }
     }
 
     @Override
     public void actionPerformed(ActionEvent e) {
-
+        if(running){
+            move();
+            checkApple();
+            checkCollisions();
+        }
+        repaint();
     }
 }
